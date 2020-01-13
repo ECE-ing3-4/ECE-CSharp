3dff3222d0cd491661840bbbb14a39976d4c973a
NeilSCGH <neil.segard@gmail.com>
2020-01-13 15:58:34 +0100

    csv ok

--- TP3/TP3/MainWindow.xaml.cs
+++ TP3/TP3/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TP3
 {
@@ -20,9 +12,37 @@ namespace TP3
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ComboBoxItem SelectedcbItem { get; set; }
+        public ObservableCollection<ComboBoxItem> cbItems { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+
+            cbItems = new ObservableCollection<ComboBoxItem>();
+            var cbItem = new ComboBoxItem { Content = "<--Pays-->" };
+            //SelectedcbItem = cbItem;
+            cbItems.Add(cbItem);
+            
+            string[] tokens;
+            char[] separators = { ',' };
+            string str = "";
+
+            FileStream fs = new FileStream(@"D:\ECE\ing4\C#\CSharp\TP3\Capital.csv", FileMode.Open);
+            StreamReader sr = new StreamReader(fs, Encoding.Default);
+
+            while ((str = sr.ReadLine()) != null)
+            {
+                tokens = str.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                cbItems.Add(new ComboBoxItem { Content = tokens[0] });
+            }
+            
+        }
+
+        private void ComboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //MessageBox.Show(sender);
         }
     }
 }

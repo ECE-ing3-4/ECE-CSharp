0e849f0755cf8baa9fb592e845b9cc470b75b869
NeilSCGH <neil.segard@gmail.com>
2020-01-13 17:01:18 +0100

    maj

--- TP3/TP3/MainWindow.xaml.cs
+++ TP3/TP3/MainWindow.xaml.cs
@@ -1,48 +1,19 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.IO;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
 
 namespace TP3
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public ComboBoxItem SelectedcbItem { get; set; }
-        public ObservableCollection<ComboBoxItem> cbItems { get; set; }
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = this;
-
-            cbItems = new ObservableCollection<ComboBoxItem>();
-            var cbItem = new ComboBoxItem { Content = "<--Pays-->" };
-            //SelectedcbItem = cbItem;
-            cbItems.Add(cbItem);
-            
-            string[] tokens;
-            char[] separators = { ',' };
-            string str = "";
-
-            FileStream fs = new FileStream(@"D:\ECE\ing4\C#\CSharp\TP3\Capital.csv", FileMode.Open);
-            StreamReader sr = new StreamReader(fs, Encoding.Default);
-
-            while ((str = sr.ReadLine()) != null)
-            {
-                tokens = str.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-
-                cbItems.Add(new ComboBoxItem { Content = tokens[0] });
-            }
-            
+            DataContext = new VM();
+            VM.run();
         }
 
-        private void ComboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void ComboBox1_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
-            //MessageBox.Show(sender);
+
         }
     }
 }
---
+++ TP3/TP3/VM.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+using System.Windows.Controls;
+
+namespace TP3
+{
+    internal class VM
+    {
+        public static ComboBoxItem SelectedcbItem { get; set; }
+        public static ObservableCollection<ComboBoxItem> cbItems { get; set; }
+        public VM()
+        {
+
+        }
+
+        public static void run()
+        {
+            cbItems = new ObservableCollection<ComboBoxItem>();
+            var cbItem = new ComboBoxItem { Content = "<--Pays-->" };
+            SelectedcbItem = cbItem;
+            cbItems.Add(cbItem);
+
+            string[] tokens;
+            char[] separators = { ',' };
+            string str = "";
+
+            FileStream fs = new FileStream(@"D:\ECE\ing4\C#\CSharp\TP3\Capital.csv", FileMode.Open);
+            StreamReader sr = new StreamReader(fs, Encoding.Default);
+
+            while ((str = sr.ReadLine()) != null)
+            {
+                tokens = str.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                cbItems.Add(new ComboBoxItem { Content = tokens[0] });
+
+            }
+        }
+        
+    }
+}

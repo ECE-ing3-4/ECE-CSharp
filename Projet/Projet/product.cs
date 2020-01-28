fd1fd749b0192d5d30c25fd859c6e9c6c1cc1fa1
NeilSCGH <neil.segard@gmail.com>
2020-01-28 09:42:58 +0100

    pour le gitignore

--- Projet/Projet/product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Projet
-{
-    public class Product
-    {
-        public int Picture { get; set; }
-        public string Name { get; set; }
-        public string Quantity { get; set; }
-    }
-}

ca63814ee7d92459c2f445f816423416c635fcdb
NeilSCGH <neil.segard@gmail.com>
2020-03-23 14:20:42 +0100

    files cleaned

---
+++ Projet/Projet/product.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Projet
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Picture_url { get; set; }
+        public string Quantity { get; set; }
+        public string Brand { get; set; }
+        public string Nutriscore { get; set; }
+        public string Ingredients { get; set; }
+        public string Code_barre { get; set; }
+
+
+
+    }
+}
---
+++ Projet/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using Projet;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void testGetProducts()
+        {
+            //Projet.MainWindow;
+            MainWindow a = new MainWindow();
+            const string url = "https://ssl-api.openfoodfacts.org/api/v0/product/3124480182708";
+            JToken tok = a.GetProducts(url);
+            tok = tok["product"]["product_name"];
+            //MessageBox.Show((string)tok);
+            string name = (string)tok;
+
+            Assert.AreEqual("Tropical", name);
+        }
+    }
+}
